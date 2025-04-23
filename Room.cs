b1f44b1e8f89a666e138b7cbd5dfc8343d6dab91
hazem-0000 <hazemsaadani2014@gmail.com>
2025-04-23 08:29:23 +0100

    Add project files.

---
+++ Room.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+public enum RoomType
+{
+	Standard,
+	Delux,
+	Suite
+}
+public enum ViewType
+{
+	Sea,
+	Forest,
+	City
+}
+public class Room
+	
+{
+	public int Id {  get; set; }
+	public RoomType Type  { get; set; }
+	public ViewType View { get; set; }
+	public decimal Price
+	{
+		get
+		{
+			return GetBasePrice() + GetViewBonus(); 
+
+		}
+	}
+	private Decimal GetBasePrice()
+	{
+		return Type switch
+		{
+			RoomType.Standard => 100,
+			RoomType.Delux => 200,
+			RoomType.Suite => 300
+		};
+	}
+		private Decimal GetViewBonus()
+		{
+			return View switch
+			{
+				ViewType.Sea => 30,
+				ViewType.Forest => 20,
+				ViewType.City => 10,
+				_=>0
+			};
+		}
+	}
+
+	
+
---
+++ RoomPricingService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class RoomPricingService
+{ 
+	public decimal CalculateTotalPrice(Room room ,int NumberOfDays,string Season)
+	{
+		Decimal baseprice=room.Price;
+		Decimal SeasonMultiplier = Season switch
+		{
+            "on-season" => 0.2m,
+			"off-season" => 0
+		};
+		return baseprice*SeasonMultiplier*NumberOfDays;
+	}
+	
+}
